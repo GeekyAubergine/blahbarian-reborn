ee794808fec428acf35f2d89755e0525ec198f7b
Zoe Aubert <hello@zoeaubert.me>
2023-10-20 13:20:47 +0100

    camera feels pretty good

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use std::{f32::consts::{E, PI}, time::Duration};
+use std::time::Duration;
 
 use bevy::{
-    prelude::*, render::texture::ImageSampler, sprite::MaterialMesh2dBundle, window::close_on_esc,
+    prelude::*,
+    window::{close_on_esc, PrimaryWindow},
 };
 use bevy_aseprite::{anim::AsepriteAnimation, AsepriteBundle, AsepritePlugin};
+use bevy_ecs_tilemap::{
+    prelude::{
+        get_tilemap_center_transform, TilemapGridSize, TilemapId, TilemapSize, TilemapTexture,
+        TilemapTileSize, TilemapType,
+    },
+    tiles::{TileBundle, TilePos, TileStorage, TileTextureIndex},
+    TilemapBundle, TilemapPlugin,
+};
 
-const CAMERA_LAG: f32 = 0.05;
+const CAMERA_OFFSET_FROM_PLAYER: f32 = 64.0;
 
 mod sprites {
     use bevy_aseprite::aseprite;
@@ -19,6 +28,9 @@ mod sprites {
 struct Player {
     speed: f32,
 }
+
+#[derive(Component)]
+struct CameraGoal;
 
 #[derive(Component)]
 struct Health {
@@ -38,7 +50,7 @@ enum Enemy {
 impl Enemy {
     fn speed(&self) -> f32 {
         match self {
-            Table => 50.0,
+            Table => 25.0,
         }
     }
 
@@ -83,23 +95,90 @@ struct Projectile {
     damage: u32,
 }
 
+fn setup_tiles(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let tiles_texture_handle: Handle<Image> = asset_server.load("tiles.png");
+
+    let map_size = TilemapSize { x: 16, y: 16 };
+
+    let tilemap_entity = commands.spawn_empty().id();
+
+    let mut tile_storage = TileStorage::empty(map_size);
+
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+            let tile_entity = commands
+                .spawn(TileBundle {
+                    position: tile_pos,
+                    tilemap_id: TilemapId(tilemap_entity),
+                    texture_index: TileTextureIndex(0),
+                    ..Default::default()
+                })
+                .id();
+
+            tile_storage.set(&tile_pos, tile_entity);
+        }
+    }
+
+    let tile_size = TilemapTileSize { x: 32.0, y: 32.0 };
+
+    let grid_size = TilemapGridSize { x: 32.0, y: 32.0 };
+
+    let map_type = TilemapType::default();
+
+    let mut transform = get_tilemap_center_transform(&map_size, &grid_size, &map_type, -1.0);
+
+    transform.translation *= 2.0;
+    transform.scale = Vec3::splat(2.0);
+
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        map_type,
+        size: map_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Single(tiles_texture_handle),
+        tile_size,
+        transform,
+        ..Default::default()
+    });
+
+    // Add atlas to array texture loader so it's preprocessed before we need to use it.
+    // Only used when the atlas feature is off and we are using array textures.
+    #[cfg(all(not(feature = "atlas"), feature = "render"))]
+    {
+        array_texture_loader.add(TilemapArrayTexture {
+            texture: TilemapTexture::Single(asset_server.load("tiles.png")),
+            tile_size,
+            ..Default::default()
+        });
+    }
+}
+
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
     commands.spawn((
+        CameraGoal,
+        Transform {
+            translation: Vec3::new(0., 0., 0.),
+            ..Default::default()
+        },
+    ));
+
+    commands.spawn((
         AsepriteBundle {
             aseprite: asset_server.load(sprites::PlayerAnim::PATH),
-            animation: AsepriteAnimation::from(sprites::PlayerAnim::tags::WALK),
+            animation: AsepriteAnimation::from(sprites::PlayerAnim::tags::IDLE_LEFT),
             transform: Transform {
-                scale: Vec3::splat(2.),
+                scale: Vec3::splat(3.),
                 translation: Vec3::new(0., 0., 0.),
                 ..Default::default()
             },
             ..Default::default()
         },
-        Player { speed: 100.0 },
+        Player { speed: 200.0 },
         Health { health: 100 },
-        Collider { radius: 32.0 },
+        Collider { radius: 42.0 },
     ));
 
     spawn_enemy(commands, asset_server);
@@ -126,9 +205,9 @@ fn spawn_enemy(mut commands: Commands, asset_server: Res<AssetServer>) {
 fn update_player_position(
     time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Player, &mut Transform)>,
+    mut player_query: Query<(&Player, &mut Transform), Without<CameraGoal>>,
 ) {
-    for (player, mut transform) in query.iter_mut() {
+    for (player, mut transform) in player_query.iter_mut() {
         let mut direction = Vec3::ZERO;
         if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
             direction.x -= 1.0;
@@ -142,12 +221,13 @@ fn update_player_position(
         if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
             direction.y += 1.0;
         }
+        direction = direction.normalize_or_zero();
         transform.translation += time.delta_seconds() * direction * player.speed;
     }
 }
 
 fn enemy_follow_player(
-    mut player_query: Query<(&Transform), (With<Player>, Without<Enemy>)>,
+    mut player_query: Query<&Transform, (With<Player>, Without<Enemy>)>,
     mut enemy_query: Query<(&Enemy, &mut Transform)>,
     time: Res<Time>,
 ) {
@@ -183,48 +263,98 @@ fn enemy_melee_player(
     }
 }
 
-fn camera_follow_player(
-    player_query: Query<&Transform, (With<Player>, Without<Camera>)>,
-    mut camera_query: Query<&mut Transform, (With<Camera>, Without<Player>)>,
+fn update_camera_goal_position(
+    player_query: Query<&Transform, (With<Player>, Without<CameraGoal>)>,
+    mut camera_goal_query: Query<&mut Transform, (With<CameraGoal>, Without<Player>)>,
+    window: Query<&Window, With<PrimaryWindow>>,
 ) {
     if let Ok(player_transform) = player_query.get_single() {
-        if let Ok(mut camera_transform) = camera_query.get_single_mut() {
-            let delta = player_transform.translation - camera_transform.translation;
-            camera_transform.translation += delta * CAMERA_LAG;
-        }
-    }
-}
+        if let Ok(mut camera_goal_transform) = camera_goal_query.get_single_mut() {
+            if let Some(position) = window.single().cursor_position() {
+                let width = window.single().width();
+                let height = window.single().height();
 
-fn spritemap_fix(mut ev_asset: EventReader<AssetEvent<Image>>, mut assets: ResMut<Assets<Image>>) {
-    for ev in ev_asset.iter() {
-        if let AssetEvent::Created { handle } = ev {
-            if let Some(texture) = assets.get_mut(handle) {
-                texture.sampler_descriptor = ImageSampler::nearest()
+                let cursor_offset_from_center = Vec3::new(
+                    position.x - width / 2.0,
+                    -(position.y - height / 2.0),
+                    0.0,
+                )
+                .clamp_length_max(CAMERA_OFFSET_FROM_PLAYER);
+
+                camera_goal_transform.translation =
+                    player_transform.translation + cursor_offset_from_center;
+            } else {
+                camera_goal_transform.translation = player_transform.translation;
             }
         }
     }
 }
 
-fn render_debug(mut gizmos: Gizmos, collider_query: Query<(&Collider, &Transform)>) {
+fn camera_move_to_goal_position(
+    camera_goal_query: Query<&Transform, (With<CameraGoal>, Without<Camera>)>,
+    mut camera_query: Query<&mut Transform, (With<Camera>, Without<CameraGoal>)>,
+) {
+    if let Ok(camera_goal_transform) = camera_goal_query.get_single() {
+        if let Ok(mut camera_transform) = camera_query.get_single_mut() {
+            let delta = camera_goal_transform.translation - camera_transform.translation;
+            camera_transform.translation += delta * 0.5;
+        }
+    }
+}
+
+// fn spritemap_fix(mut ev_asset: EventReader<AssetEvent<Image>>, mut assets: ResMut<Assets<Image>>) {
+//     for ev in ev_asset.iter() {
+//         if let AssetEvent::Created { handle } = ev {
+//             if let Some(texture) = assets.get_mut(handle) {
+//                 texture.sampler_descriptor = ImageSampler::nearest()
+//             }
+//         }
+//     }
+// }
+
+fn render_debug(
+    mut gizmos: Gizmos,
+    collider_query: Query<(&Collider, &Transform)>,
+    camera_goal_position_query: Query<&Transform, With<CameraGoal>>,
+) {
     for (collider, tranform) in collider_query.iter() {
         gizmos.circle_2d(tranform.translation.truncate(), collider.radius, Color::RED);
+    }
+
+    if let Ok(camera_goal_transform) = camera_goal_position_query.get_single() {
+        gizmos.circle_2d(
+            camera_goal_transform.translation.truncate(),
+            10.0,
+            Color::GREEN,
+        );
     }
 }
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Blahbarian".to_owned(),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
         .add_plugins(AsepritePlugin)
+        .add_plugins(TilemapPlugin)
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, setup_tiles))
         .add_systems(
             Update,
             (
-                spritemap_fix,
                 update_player_position,
-                camera_follow_player,
                 enemy_melee_player,
                 enemy_follow_player,
+                update_camera_goal_position,
+                camera_move_to_goal_position,
                 #[cfg(debug_assertions)]
                 close_on_esc,
                 #[cfg(debug_assertions)]
